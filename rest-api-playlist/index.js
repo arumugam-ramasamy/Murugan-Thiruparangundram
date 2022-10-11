55f7ee91a104421a390683d0b8e58d556884bd85
arumugam-ramasamy <sundar_hariharane@yahoo.com>
2022-10-11 11:10:16 -0700

    cleaned

--- rest-api-playlist/index.js
+++ rest-api-playlist/index.js
@@ -1,11 +1,22 @@
 const express = require ('express') ;
-const routes = require ('./routes/ninjaRoutes') ;
+const bodyParser = require('body-parser') ;
+const mongoose = require('mongoose');
 
 //set up express app 
 const app = express() ;
-app.use('/api', routes) ;
+app.use(bodyParser.json()) ;
+app.use('/api',  require('./routes/ninjaRoutes')) ;
+app.use(express.static('public')) ;
+app.use(function(err, req, res, next) {
+	console.log(err) ;
+	res.status(422).send({error:err.message}) ;
+});
 
-
+//const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI= 'mongodb+srv://sundarhariharane:Tharun12!@cluster0.uzjjbm5.mongodb.net/rest-api' ;
+mongoose.connect(dbURI) ;
+mongoose.Promise = global.Promise ;
+ 
 app.listen(process.env.port || 8000, function () {
 	console.log('now listening for requests') ;
 }) ;
--- rest-api-playlist/routes/ninjaRoutes.js
+++ rest-api-playlist/routes/ninjaRoutes.js
@@ -1,23 +1,49 @@
 const express = require('express') ;
+const Ninja = require('../models/ninja') ;
 
 const router = express.Router() ;
 
-router.get('/ninjas', function (req, res) {
-	console.log("I am in get") ;
-	res.send({type : 'GET'}) ;
+router.get('/ninjas', function (req, res, next) {
+	/*Ninja.find({}).then(function (ninjas){
+		res.send(ninjas) ;
+	});*/
+	console.log ("came here") ;
+	Ninja.aggregate()
+    .near({
+      near: {
+        type: "point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      maxDistance: 100000,
+      spherical: true
+    })
+    .then(function(ninjas) {
+    	console.log(ninjas) ;
+      res.send(ninjas);
+    })
+    .catch(next);
 }) ;
 
-router.post('/ninjas', function (req, res) {
-	console.log("post request received") ;
-	res.send({type : 'POST'}) ;
+router.post('/ninjas', function (req, res, next) {
+	Ninja.create(req.body).then(function(ninja) {
+		res.send(ninja) ;
+	}).catch(next) ;
 }) ;
 
-router.put('/ninjas/:id', function (req, res) {
-	res.send({type : 'PUT'}) ;
+router.put('/ninjas/:id', function (req, res, next) {
+	Ninja.findByIdAndUpdate({_id : req.params.id}, req.body).then (function () {}) ;
+	Ninja.findOne({_id : req.params.id}).then (function(ninja) {
+		res.send(ninja) ;
+	}) ;
+	
 }) ;
 
-router.delete('/ninjas/:id', function (req, res) {
-	res.send({type : 'DELETE'}) ;
+router.delete('/ninjas/:id', function (req, res, next) {
+	Ninja.findByIdAndRemove({_id : req.params.id}).then (function(ninja) {
+		res.send(ninja) ;
+	}) ;
+	//res.send({type : 'DELETE'}) ;
 }) ;
 
 module.exports = router ;
